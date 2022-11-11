82029c84735a1926cf6c7eebee336fb53f712f66
Erik <erik.vainumae@gmail.com>
2022-11-11 17:59:47 +0200

    initial commit

---
+++ src/test/java/com/team5/teamscoretrackerserver/TeamScoreTrackerServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team5.teamscoretrackerserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamScoreTrackerServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
