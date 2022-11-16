1dc64f9906bc07667eac41f1f63e26f6746094c8
Lungu Marius <Marius.Lungu@solera.com>
2022-11-16 14:32:12 +0100

    -editing the activity score

--- src/main/java/com/team5/teamscoretrackerserver/controller/ActivityController.java
+++ src/main/java/com/team5/teamscoretrackerserver/controller/ActivityController.java
@@ -1,10 +1,13 @@
 package com.team5.teamscoretrackerserver.controller;
 
+import com.team5.teamscoretrackerserver.exeptions.TeamNotFoundException;
 import com.team5.teamscoretrackerserver.model.Activity;
 import com.team5.teamscoretrackerserver.model.Team;
 import com.team5.teamscoretrackerserver.service.ActivityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.math.BigInteger;
 
 @RestController
 @RequestMapping("/activities")
@@ -13,8 +16,10 @@ public class ActivityController {
     @Autowired
     ActivityService activityService;
 
-    @PutMapping("/edit")
-    public void editActivityScore(@RequestBody Activity activity){
-         activityService.editActivityScore(activity);
+    @PutMapping("teamId={teamId}&activityId={activityId}&score={score}")
+    public void editActivityScore(@PathVariable("teamId") BigInteger teamId,
+                                  @PathVariable("activityId") BigInteger activityId,
+                                  @PathVariable("score") Integer score) throws TeamNotFoundException {
+         activityService.editActivityScore(teamId,activityId,score);
     }
 }
--- src/main/java/com/team5/teamscoretrackerserver/service/ActivityService.java
+++ src/main/java/com/team5/teamscoretrackerserver/service/ActivityService.java
@@ -1,11 +1,14 @@
 package com.team5.teamscoretrackerserver.service;
 
+import com.team5.teamscoretrackerserver.exeptions.TeamNotFoundException;
 import com.team5.teamscoretrackerserver.model.Activity;
 import com.team5.teamscoretrackerserver.model.Team;
 import com.team5.teamscoretrackerserver.repository.TeamRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -15,36 +18,33 @@ import java.util.Optional;
 public class ActivityService {
 
     @Autowired
+    private TeamService teamService;
+    @Autowired
     private TeamRepository teamRepository;
 
-    public void editActivityScore(Activity activity) {
+    public void editActivityScore(BigInteger teamId, BigInteger activityId, Integer score) throws TeamNotFoundException {
 
 
         //We are given an activity from the frontend and searching in all teams to find out which team has this activity.
         // I know it's bullshit but i ll find a better way
 
-        List<Team> teams = teamRepository.findAll();
+        Team team = teamService.findTeamById(teamId);
 
-
-        if (teams != null) {
-            //searching in the team's activities to find and edit the activity
-        teams.forEach(team -> {
             List<Activity> activityList = team.getActivityList();
             activityList.forEach(activity1 -> {
-                if (activity1.getActivityId() == activity.getActivityId()) {
-
-                    activity1.setScore(activity.getScore());
+                if (activity1.getActivityId().equals(activityId)) {
+                    activity1.setScore(score);
                     team.setActivityList(activityList);
                 }
         });
 
                 //finally saving the team along with the activity update in the repository
                 teamRepository.save(team);
-            });
+            }
 
 
-        }
-    }
+
+
 
 
 }
