41aef33a998820179e81c073ac89ede8bcb420c1
Erik <erik.vainumae@gmail.com>
2022-11-17 17:50:39 +0200

    finished implementing last methods

--- src/test/java/com/team5/teamscoretrackerserver/service/TeamServiceTest.java
+++ src/test/java/com/team5/teamscoretrackerserver/service/TeamServiceTest.java
@@ -1,5 +1,6 @@
 package com.team5.teamscoretrackerserver.service;
 
+import com.team5.teamscoretrackerserver.exeptions.TeamNotFoundException;
 import com.team5.teamscoretrackerserver.model.Team;
 import com.team5.teamscoretrackerserver.repository.TeamRepository;
 import org.junit.jupiter.api.BeforeEach;
@@ -8,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
+import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -25,14 +27,13 @@ class TeamServiceTest {
     Team team;
 
     @BeforeEach
-    public void beforeEach(){
+    public void beforeEach() {
         team = new Team("A-Team", 69);
     }
 
 
-
     @Test
-    void addTeam(){
+    void addTeam() {
         when(teamRepository.save(team)).thenReturn(team);
         assertEquals(teamService.convertEntityToDTO(team), teamService.addTeam(team));
 
@@ -40,21 +41,28 @@ class TeamServiceTest {
 
     @Test
     void getAllTeams() {
-        when(teamRepository.findAll()).thenReturn(Stream.of(
-                new Team("A-Team", 69), new Team("Fantastic Five", 5))
+        when(teamRepository.findAll()).thenReturn(Stream.of(team, team)
                 .collect(Collectors.toList()));
         assertEquals(2, teamService.getAllTeams().size());
     }
 
     @Test
     void getTeamDTO() {
+        when(teamRepository.findAll()).thenReturn(Stream.of(team, team)
+                .collect(Collectors.toList()));
+        assertEquals(teamService.getTeamDTO().size(), 2);
     }
 
     @Test
-    void findTeamByName() {
+    void findTeamByName() throws TeamNotFoundException {
+        when(teamRepository.findTeamByTeamName(team.getTeamName())).thenReturn(Optional.ofNullable(team));
+        assertEquals(team, teamService.findTeamByName(team.getTeamName()));
+
     }
 
     @Test
-    void findTeamById() {
+    void findTeamById() throws TeamNotFoundException {
+        when(teamRepository.findById(team.getTeamId())).thenReturn(Optional.ofNullable(team));
+        assertEquals(team, teamService.findTeamById(team.getTeamId()));
     }
 }
